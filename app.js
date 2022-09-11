3b188d205c2cd77c4f01ebeb49e0ef4567d3a6ee
Zackary Entwistle <zackaryentwistle@gmail.com>
2022-09-11 17:53:18 -0400

    done

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
+  console.log(response);
 
   // Return the response in JSON format
   return response.json();
@@ -26,15 +26,22 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  const fruits = fruitsArray.filter((item) => item.nutritions.protein > 1);
+
+  console.log(fruits);
 
   // TODO: Create a new HTML element to display your data
-
   // TODO: Append your new element to the page
-
+  fruits.forEach(fruit => {
+    const element = document.createElement("p");
+    const content = document.createTextNode(fruit.name);
+    element.append(content);
+    document.body.insertBefore(element, gallery);
+  });
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
